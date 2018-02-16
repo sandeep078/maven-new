31a1b7f9b0fa3cb64a147efeb8e10601a414bce0
sandeep078 <sandeep.smart078@gmail.com>
2018-02-16 17:17:33 +0000

    more folders

---
+++ generator/lcd_chef/files/default/spec_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require 'chefspec'
+require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+  config.platform = 'ubuntu'
+end
+  
---
+++ generator/lcd_chef/metadata.rb
@@ -0,0 +1,5 @@
+name             'lcd_chef'
+description      'Custom code generator cookbook for use with ChefDK'
+long_description 'Custom code generator cookbook for use with ChefDK'
+version          '0.1.0'
+
